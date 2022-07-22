17eed0a11d23b72c56ec3d64d2eeb5f5ebfb91c2
deepshi-jindal <97787082+deepshi-jindal@users.noreply.github.com>
2022-07-22 17:09:34 +0530

    Time: 424 ms (29.59%), Space: 113.9 MB (93.63%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -8,10 +8,10 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+/*class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        ListNode *slow=head;
+      /*  ListNode *slow=head;
         stack<int> s;
         while(slow!=NULL){
             s.push(slow->val);
@@ -23,6 +23,74 @@ public:
             if(head->val != i)
                 return false;
             head=head->next;
+        }
+        return true;
+     //   if(head==NULL || head->next==NULL)
+       //     return true;
+        ListNode *slow=head;
+        ListNode *fast=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        slow->next=reverseList(slow->next);
+        slow=slow->next;
+        
+        while(slow){
+            if(head->val!=slow->val)
+                return false;
+            head=head->next;
+            slow=slow->next;
+        }
+        return true;
+    }
+        
+        ListNode *reverseList(ListNode *head){
+            ListNode *prev=NULL;
+            ListNode *curr=head;
+            ListNode *next;
+            while(curr){
+                next=curr->next;
+                curr->next=prev;
+                prev=curr;
+                curr=next;
+            }
+            return prev;
+        }
+    };*/
+class Solution {
+public:
+    bool isPalindrome(ListNode* head)
+    {
+        // finding the mid
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        // reversing the list from mid
+        ListNode* ptr = NULL;
+        ListNode* next = NULL;
+        while(slow != NULL)
+        {
+            next = slow->next;
+            slow->next = ptr;
+            ptr = slow;
+            slow = next;
+        }
+        
+        // finally checking the elements with the reversed mid elements.
+        while(ptr != NULL)
+        {
+            if(head->val != ptr->val)
+            {
+                return false;
+            }
+            head = head->next;
+            ptr = ptr->next;
         }
         return true;
     }
