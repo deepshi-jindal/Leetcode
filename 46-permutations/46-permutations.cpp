c54ba1acfe4674027cff6090901c6702d5daef72
Deepshi jindal <97787082+deepshi-jindal@users.noreply.github.com>
2022-08-07 14:10:10 +0530

    Time: 4 ms (66.43%), Space: 7.8 MB (54.56%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     vector<vector<int>>ans;
     void permutations (vector<int> &nums ,int index){
@@ -17,4 +17,16 @@ public:
         permutations(nums,0);
         return ans;
     }
-};
+};*/
+
+class Solution {
+public:
+     vector<vector<int>>ans;
+    vector<vector<int>> permute(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        do{
+            ans.push_back(nums);
+        }while(next_permutation(nums.begin(),nums.end()));
+        return ans;
+               }
+      };
