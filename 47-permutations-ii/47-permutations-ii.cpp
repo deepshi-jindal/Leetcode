20a821e1c49b2d2f12601152d5730ac75e30b9da
Deepshi jindal <97787082+deepshi-jindal@users.noreply.github.com>
2022-08-07 14:30:00 +0530

    Time: 4 ms (94.13%), Space: 8.4 MB (91.05%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
       vector<vector<int>>ans;
     void permutations(vector<int> nums ,int index){
@@ -18,6 +18,20 @@ public:
       sort(nums.begin(),nums.end());
       
         permutations(nums,0);
+        return ans;
+    }
+};*/
+
+
+class Solution {
+public:
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+      sort(nums.begin(),nums.end());
+          vector<vector<int>>ans;
+           ans.push_back(nums);
+        while(next_permutation(nums.begin(),nums.end()))
+            ans.push_back(nums);
         return ans;
     }
 };
