d25510d8727f29e9e45ef0a08160dee27e1d9e9d
Deepshi jindal <97787082+deepshi-jindal@users.noreply.github.com>
2022-08-09 21:54:36 +0530

    Time: 2 ms (62.02%), Space: 5.7 MB (6.38%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.c
@@ -0,0 +1,14 @@
+
+
+bool isPowerOfTwo(int n){
+      if(n<=0)
+           return false;
+     if(n==1)
+         return true;
+   while(n%2==0)
+       n=n/2;
+    return (n==1);
+/*
+    return (n>0 && (n &(n-1)==0));
+    */
+}
