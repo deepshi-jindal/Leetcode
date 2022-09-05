1027159caca44d1a549c5f7c7ef8b0606c4a2bfb
Deepshi jindal <97787082+deepshi-jindal@users.noreply.github.com>
2022-09-05 17:31:49 +0530

    Time: 10 ms (13.88%), Space: 8 MB (33.65%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> answer;
+        for(int i=1;i<=n;i++){
+            string str;
+            if(i%3==0)
+                str+="Fizz";
+             if(i%5==0)
+                str+="Buzz";
+            if(str.empty())
+                str=to_string(i);
+       answer.push_back(str);
+        }
+        return answer;
+    }
+};
