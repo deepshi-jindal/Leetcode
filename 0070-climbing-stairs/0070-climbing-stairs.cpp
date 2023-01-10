90cdf8a607f159775feeca29137a9a05d0fcdace
Deepshi jindal <97787082+deepshi-jindal@users.noreply.github.com>
2023-01-10 22:57:10 +0530

    Time: 3 ms (35.51%), Space: 6 MB (40.63%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+   // int dp[n];
+    int climbStairs(int n) {
+        int a=1,b=1,c=0,i;
+        for(int i=2;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+};
