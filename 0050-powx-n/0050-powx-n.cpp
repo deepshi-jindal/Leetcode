dd003c675c0f8d7b40c3e53c087126a65aab9d7d
Deepshi jindal <97787082+deepshi-jindal@users.noreply.github.com>
2023-07-24 10:19:38 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (92.36%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double y=(double)pow(x,n);
-        return y;
+        // double y=(double)pow(x,n);
+        // return y;
+        if(n==0)
+            return 1;
+       // x^n=x^n/2*x^n/2
+        double y=myPow(x,n/2);
+        if(n%2==0)
+            return y*y;
+        else if(n>0)
+            return x*y*y;
+        else
+            return (1/x)*y*y;
     }
 };
