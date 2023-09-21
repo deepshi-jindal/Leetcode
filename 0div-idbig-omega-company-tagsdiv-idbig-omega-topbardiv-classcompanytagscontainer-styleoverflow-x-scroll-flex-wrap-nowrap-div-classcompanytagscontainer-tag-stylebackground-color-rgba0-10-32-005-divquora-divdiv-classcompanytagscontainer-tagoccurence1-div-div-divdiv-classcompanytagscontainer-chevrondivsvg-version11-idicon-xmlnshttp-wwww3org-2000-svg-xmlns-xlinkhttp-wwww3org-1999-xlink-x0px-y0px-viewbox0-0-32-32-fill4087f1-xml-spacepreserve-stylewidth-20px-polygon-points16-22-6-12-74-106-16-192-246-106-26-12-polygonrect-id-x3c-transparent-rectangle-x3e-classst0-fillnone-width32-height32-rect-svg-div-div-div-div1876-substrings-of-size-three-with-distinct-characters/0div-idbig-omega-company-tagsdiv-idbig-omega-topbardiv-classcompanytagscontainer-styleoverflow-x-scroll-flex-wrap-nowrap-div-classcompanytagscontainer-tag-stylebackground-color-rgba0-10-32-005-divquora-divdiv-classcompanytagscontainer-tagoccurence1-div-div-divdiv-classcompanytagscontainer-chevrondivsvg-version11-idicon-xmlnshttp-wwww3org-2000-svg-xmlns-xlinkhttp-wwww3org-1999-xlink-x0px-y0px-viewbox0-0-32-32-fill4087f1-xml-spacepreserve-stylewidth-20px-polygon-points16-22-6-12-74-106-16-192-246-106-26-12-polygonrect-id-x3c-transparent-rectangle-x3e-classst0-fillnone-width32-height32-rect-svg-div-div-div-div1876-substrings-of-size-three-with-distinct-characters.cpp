b565fd159c3a2c6185942e537c1d993f2691ff1d
Deepshi jindal <97787082+deepshi-jindal@users.noreply.github.com>
2023-09-21 21:17:02 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (54.99%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divquora-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1876-substrings-of-size-three-with-distinct-characters/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divquora-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divquora-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1876-substrings-of-size-three-with-distinct-characters/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divquora-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
     int countGoodSubstrings(string s) {
-        int i=0,j=0;
-        int n=s.size();
-        int ans=0;
-        while(j<n){
-            if(j-i+1==3){
-                string temp=s.substr(i,3);
-                map<int,int>mp;
-                for(auto i:temp){
-                    mp[i]++;
-                }
-                if(mp.size()==3){
-                    ans++;
-                }
-                i++;
+        // int i=0,j=0;
+        // int n=s.size();
+        // int ans=0;
+        // while(j<n){
+        //     if(j-i+1==3){
+        //         string temp=s.substr(i,3);
+        //         map<int,int>mp;
+        //         for(auto i:temp){
+        //             mp[i]++;
+        //         }
+        //         if(mp.size()==3){
+        //             ans++;
+        //         }
+        //         i++;
+        //     }
+        //     j++;
+        // }
+        // return ans;
+        int i=0,j=0,count=0;
+        while(j<s.size()){
+            if(j-i+1 < 3)
+                j++;
+            else if(j-i+1 == 3){
+               if((s[i]!=s[i+1] && s[i]!=s[i+2]) && (s[i+1]!=s[i] && s[i+1]!=s[i+2]) && (s[i+2]!=s[i+1] && s[i+2]!=s[i])) 
+                   count++;
+                i++;j++;
             }
-            j++;
         }
-        return ans;
+        return count;
     }
 };
