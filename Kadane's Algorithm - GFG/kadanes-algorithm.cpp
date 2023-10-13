ae6994a6068b4341dbf75618b0ce2cdaac703da9
Deepshi jindal <97787082+deepshi-jindal@users.noreply.github.com>
2023-10-13 16:09:35 +0530

    Added solution - LeetHub

--- Kadane's Algorithm - GFG/kadanes-algorithm.cpp
+++ Kadane's Algorithm - GFG/kadanes-algorithm.cpp
@@ -16,11 +16,11 @@ class Solution{
         long long maxi=INT_MIN;
         for(int i=0;i<n;i++){
             sum+=arr[i];
-        
+
         if(sum>maxi)
             maxi=sum;
-            if(sum<0)
-                sum=0;            
+                  if(sum<0)
+                sum=0;              
         
         }
         return maxi;
