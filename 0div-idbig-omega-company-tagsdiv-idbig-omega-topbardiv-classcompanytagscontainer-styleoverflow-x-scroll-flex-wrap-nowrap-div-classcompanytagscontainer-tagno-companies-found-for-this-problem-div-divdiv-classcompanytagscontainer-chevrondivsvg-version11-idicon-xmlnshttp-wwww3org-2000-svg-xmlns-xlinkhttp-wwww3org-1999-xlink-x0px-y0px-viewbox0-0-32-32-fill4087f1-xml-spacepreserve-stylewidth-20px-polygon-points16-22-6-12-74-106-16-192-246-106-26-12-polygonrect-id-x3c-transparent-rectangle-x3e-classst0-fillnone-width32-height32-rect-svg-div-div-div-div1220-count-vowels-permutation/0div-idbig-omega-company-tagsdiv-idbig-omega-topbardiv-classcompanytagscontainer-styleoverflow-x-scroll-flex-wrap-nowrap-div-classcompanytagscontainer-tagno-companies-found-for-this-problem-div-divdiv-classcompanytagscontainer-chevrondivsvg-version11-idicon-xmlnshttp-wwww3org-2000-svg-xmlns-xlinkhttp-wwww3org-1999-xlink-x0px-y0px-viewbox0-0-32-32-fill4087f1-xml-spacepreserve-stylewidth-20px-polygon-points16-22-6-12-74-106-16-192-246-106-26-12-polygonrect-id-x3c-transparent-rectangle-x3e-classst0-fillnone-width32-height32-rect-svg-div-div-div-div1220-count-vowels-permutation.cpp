e45c99990d50d7d0daa346d2911039117563d44d
Deepshi jindal <97787082+deepshi-jindal@users.noreply.github.com>
2023-10-28 17:24:05 +0530

    Time: 3 ms (92.00%), Space: 6.4 MB (84.16%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1220-count-vowels-permutation/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1220-count-vowels-permutation.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countVowelPermutation(int n) {
+        long a=1 ,e=1, i=1, o=1, u=1;
+        long mod= 1e9+7;
+        for(int j=2;j<=n;j++){
+            long a2=(e +i+ u)% mod;
+            long e2=(a+i)%mod;
+            long i2=(e+o)%mod;
+            long o2=(i);
+            long u2=(i+o)%mod;
+            
+            a=a2,e=e2,i=i2,o=o2,u=u2;
+        }
+        return(a+e+i+o+u)%mod;
+    }
+};
