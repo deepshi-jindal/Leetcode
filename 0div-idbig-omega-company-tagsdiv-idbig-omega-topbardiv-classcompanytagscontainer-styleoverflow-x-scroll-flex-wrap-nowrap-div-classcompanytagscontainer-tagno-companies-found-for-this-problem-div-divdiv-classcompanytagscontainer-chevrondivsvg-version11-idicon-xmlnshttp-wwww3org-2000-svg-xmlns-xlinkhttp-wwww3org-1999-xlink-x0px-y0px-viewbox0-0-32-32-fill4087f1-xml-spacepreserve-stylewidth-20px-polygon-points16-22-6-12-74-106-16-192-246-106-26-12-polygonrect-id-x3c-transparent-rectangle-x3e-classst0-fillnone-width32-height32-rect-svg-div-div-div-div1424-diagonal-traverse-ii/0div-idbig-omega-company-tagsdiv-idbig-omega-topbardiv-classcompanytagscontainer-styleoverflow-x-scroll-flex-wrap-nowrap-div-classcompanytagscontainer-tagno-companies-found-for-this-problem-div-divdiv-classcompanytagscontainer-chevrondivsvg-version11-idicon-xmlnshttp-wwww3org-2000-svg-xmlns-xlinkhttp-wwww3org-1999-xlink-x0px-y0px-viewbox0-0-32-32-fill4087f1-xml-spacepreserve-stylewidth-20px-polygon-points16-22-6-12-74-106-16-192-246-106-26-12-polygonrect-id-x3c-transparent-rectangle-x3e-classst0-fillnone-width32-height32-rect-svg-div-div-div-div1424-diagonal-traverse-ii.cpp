aaca51625aba8aeddf38359c0192daa136674d12
Deepshi jindal <97787082+deepshi-jindal@users.noreply.github.com>
2023-11-22 20:16:29 +0530

    Time: 112 ms (94.23%), Space: 65.9 MB (99.57%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1424-diagonal-traverse-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1424-diagonal-traverse-ii.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1424-diagonal-traverse-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1424-diagonal-traverse-ii.cpp
@@ -1,22 +1,43 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
-       int maxsum=0;
-        vector<int>ans;
-        unordered_map<int,vector<int>>sumtoelements;
+//        int maxsum=0;
+//         vector<int>ans;
+//         unordered_map<int,vector<int>>sumtoelements;
         
-        for(int i=nums.size()-1;i>=0;i--){
-            for(int j=0;j<nums[i].size();j++){
-                maxsum=max(maxsum,i+j);
-                sumtoelements[i+j].push_back(nums[i][j]);
+//         for(int i=nums.size()-1;i>=0;i--){
+//             for(int j=0;j<nums[i].size();j++){
+//                 maxsum=max(maxsum,i+j);
+//                 sumtoelements[i+j].push_back(nums[i][j]);
+//             }
+//         }
+//         for(int i=0;i<=maxsum;i++){
+//             for(auto e: sumtoelements[i]){
+//                 ans.push_back(e);         
+//             }
+        
+//     }
+//     return ans;
+        vector<int>abs;
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        
+        while(!q.empty()){
+            int size=q.size();
+            while(size--){
+                auto[i,j]=q.front();
+                q.pop();
+                
+                abs.push_back(nums[i][j]);
+                
+                if(j==0 && i+1<nums.size()){
+                    q.push({i+1,j});
+                }
+                if(j+1<nums[i].size()){
+                    q.push({i,j+1});
+                }
             }
         }
-        for(int i=0;i<=maxsum;i++){
-            for(auto e: sumtoelements[i]){
-                ans.push_back(e);         
-            }
-        
-    }
-    return ans;
+        return abs;
 }
 };
