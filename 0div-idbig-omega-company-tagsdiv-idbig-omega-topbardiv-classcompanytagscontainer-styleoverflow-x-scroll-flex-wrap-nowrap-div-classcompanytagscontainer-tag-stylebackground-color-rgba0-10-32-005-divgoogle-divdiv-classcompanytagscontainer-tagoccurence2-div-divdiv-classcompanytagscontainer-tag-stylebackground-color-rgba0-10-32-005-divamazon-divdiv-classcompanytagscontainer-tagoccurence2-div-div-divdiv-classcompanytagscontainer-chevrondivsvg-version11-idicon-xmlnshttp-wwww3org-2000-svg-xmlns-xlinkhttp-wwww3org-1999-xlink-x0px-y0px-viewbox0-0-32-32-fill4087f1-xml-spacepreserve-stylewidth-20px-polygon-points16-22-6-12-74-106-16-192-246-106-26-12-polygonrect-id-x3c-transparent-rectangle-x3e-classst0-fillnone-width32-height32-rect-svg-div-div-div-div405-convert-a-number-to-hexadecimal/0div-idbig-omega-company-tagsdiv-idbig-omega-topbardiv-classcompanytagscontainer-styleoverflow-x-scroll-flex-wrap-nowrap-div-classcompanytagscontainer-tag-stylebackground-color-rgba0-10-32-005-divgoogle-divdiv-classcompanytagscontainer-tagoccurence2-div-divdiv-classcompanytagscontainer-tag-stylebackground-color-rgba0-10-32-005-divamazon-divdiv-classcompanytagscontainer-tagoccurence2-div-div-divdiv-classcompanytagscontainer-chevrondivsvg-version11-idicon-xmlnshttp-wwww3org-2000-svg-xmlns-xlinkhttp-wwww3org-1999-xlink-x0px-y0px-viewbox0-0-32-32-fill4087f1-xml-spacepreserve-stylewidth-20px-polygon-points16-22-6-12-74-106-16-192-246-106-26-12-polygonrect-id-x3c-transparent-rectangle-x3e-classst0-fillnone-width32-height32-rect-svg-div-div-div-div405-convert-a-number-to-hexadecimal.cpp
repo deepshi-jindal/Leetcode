5249564ec4106ff743bfe428bc74f10ce4f63715
Deepshi jindal <97787082+deepshi-jindal@users.noreply.github.com>
2023-12-30 22:46:18 +0530

    Time: 3 ms (14.50%), Space: 6.3 MB (85.96%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div405-convert-a-number-to-hexadecimal/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div405-convert-a-number-to-hexadecimal.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    string toHex(int num)
+    {
+        // string hex = "0123456789abcdef";
+        // unsigned int n = num; // to handle neg numbers
+        // string ans = "";
+        // if (n == 0)
+        //     return "0";
+        // while (n > 0)
+        // {
+        //     int k = n % 16;
+        //     ans += hex[k];
+        //     n /= 16;
+        // }
+        // reverse(ans.begin(), ans.end()); // as we stored it in the opposite order
+        // return ans;
+        string hex="0123456789abcdef";
+        unsigned int n=num;
+        string ans="";
+        if(n==0)
+            return "0";
+        while(n>0){
+            int k=n%16;
+            ans+=hex[k];
+            n=n/16;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
